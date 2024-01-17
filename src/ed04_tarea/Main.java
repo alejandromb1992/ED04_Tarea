d0b66dcd2331c9a2e61e7a2ea73012658693db63
bolierixon <alexbq92@gmail.com>
2024-01-17 18:48:46 +0100

    Modificación 1: Añadir código.

--- src/ed04_tarea/Main.java
+++ src/ed04_tarea/Main.java
@@ -9,52 +9,54 @@ package ed04_tarea;
  *
  * @author Alejandro Martín Belmonte
  */
-public class Main { 
+public class Main {
 
     public static void main(String[] args) {
 
         eCesta cesta;
 
-        cesta = new eCesta("Apellidos del alumno",100, "dni del alumno", 10);
+        cesta = new eCesta("Apellidos del alumno", 100, "dni del alumno", 10);
 
-        NuevoMetodo(cesta); 
+        NuevoMetodo(cesta);
 
     }
 
     public static void NuevoMetodo(eCesta cesta) {
-        try
-            
-        {
-            
+        try {
+
             cesta.comprar(50);
 
-        } catch (Exception e)
-
-        {
+        } catch (Exception e) {
 
             System.out.println("No se puede pagar  ");
 
         }
 
-        try
-
-        {
+        try {
 
             System.out.println("Solicitar actualización de bono");
 
             cesta.ActualizarBono(100);
 
-        } catch (Exception e)
-
-        {
+        } catch (Exception e) {
 
             System.out.println("Error al recargar");
 
         }
+        try {
 
+            System.out.println(" Modificación 1");
+
+            cesta.ActualizarBono(-50);
+
+        } catch (Exception e) {
+
+            System.out.print("Fallo…….");
+
+        }
         double bonoactual = cesta.bonificacion();
 
-        System.out.println("Su bono actual es  "+ bonoactual );
+        System.out.println("Su bono actual es  " + bonoactual);
     }
 
 }
