8d7e787de8fd755407fecf537ec47060afc835d2
Alejandro Martín <alexbq92@gmail.com>
2024-01-17 18:24:31 +0100

    Inicio

---
+++ src/ed04_tarea/Main.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed04_tarea;
+
+/**
+ *
+ * @author Alejandro Martín Belmonte
+ */
+public class Main { 
+
+    public static void main(String[] args) {
+
+        eCesta cesta;
+
+        cesta = new eCesta("Apellidos del alumno",100, "dni del alumno", 10);
+
+        NuevoMetodo(cesta); 
+
+    }
+
+    public static void NuevoMetodo(eCesta cesta) {
+        try
+            
+        {
+            
+            cesta.comprar(50);
+
+        } catch (Exception e)
+
+        {
+
+            System.out.println("No se puede pagar  ");
+
+        }
+
+        try
+
+        {
+
+            System.out.println("Solicitar actualización de bono");
+
+            cesta.ActualizarBono(100);
+
+        } catch (Exception e)
+
+        {
+
+            System.out.println("Error al recargar");
+
+        }
+
+        double bonoactual = cesta.bonificacion();
+
+        System.out.println("Su bono actual es  "+ bonoactual );
+    }
+
+}
